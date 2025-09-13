47a8da4f5b301baad838224a4175da1c34242cc1
Tim Lin <lintim03@gmail.com>
2025-09-13 14:04:57 -0500

    Rework Unit to use Composition, decouple Movement

--- Assets/Scripts/GamePlay/Unit.cs
+++ Assets/Scripts/GamePlay/Unit.cs
@@ -1,40 +1,41 @@
 using Source.Shared.Utilities;
 using System;
 using UnityEngine;
-using UnityEngine.AI;
 
 namespace Source.GamePlay
 {
     public class Unit : MonoBehaviour
     {
+        [SerializeField]
+        private UnitMovement UnitMovement;
+
         [InitializationRequired]
-        private NavMeshAgent NavMeshAgent;
-
-        private int Health;
-        private int Range;
-
+        private float? Health = null;
         [InitializationRequired]
         public Guid? PlayerId { get; private set; } = null;
         public bool Selected { get; private set; } = false;
-
-        private void Awake()
-        {
-            NavMeshAgent = GetComponent<NavMeshAgent>();
-        }
 
         private void Start()
         {
             this.CheckInitializeRequired();
         }
 
-        public void Initialize(Guid playerId)
+        public void Initialize(Guid playerId, float health, float speed)
         {
             PlayerId = playerId;
+            Health = health;
+            if (UnitMovement != null)
+            {
+                UnitMovement.Initialize(speed);
+            }
         }
 
         public void MoveUnit(Vector3 destination)
         {
-            NavMeshAgent.SetDestination(destination);
+            if (UnitMovement == null)
+            {
+                UnitMovement.MoveUnit(destination);
+            }
         }
 
         public void Select()
