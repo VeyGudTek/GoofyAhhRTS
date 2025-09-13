c35aa988cce5d6f0e6eef32f5be0ad2797529f14
Tim Lin <lintim03@gmail.com>
2025-09-13 00:00:16 -0500

    Set Up Methods for Unit Selection

--- Assets/Scripts/GamePlay/Unit.cs
+++ Assets/Scripts/GamePlay/Unit.cs
@@ -1,9 +1,49 @@
+using Source.Shared.Utilities;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Source.GamePlay
 {
+    public enum UnitOwner
+    {
+        Player,
+        Enemy
+    }
+
     public class Unit : MonoBehaviour
     {
-        private int health;
+        [InitializationRequired]
+        private NavMeshAgent NavMeshAgent;
+
+        private int Health;
+        private int Range;
+
+        public UnitOwner Owner { get; private set; } = UnitOwner.Player;
+        public bool Selected { get; private set; } = false;
+
+        private void Awake()
+        {
+            NavMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Start()
+        {
+            this.CheckInitializeRequired();
+        }
+
+        public void MoveUnit(Vector3 destination)
+        {
+            NavMeshAgent.SetDestination(destination);
+        }
+
+        public void Select()
+        {
+            Selected = true;
+        }
+
+        public void Deselect()
+        {
+            Selected = false;
+        }
     }
 }
--- Assets/Scripts/GamePlay/UnitManager.cs
+++ Assets/Scripts/GamePlay/UnitManager.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace Source.GamePlay
 {
     public class UnitManager : MonoBehaviour
     {
-        [SerializeField]
-        private List<Unit> PlayerUnits = new List<Unit>();
-
-        public void OnClick()
+        private List<Unit> Units = new List<Unit>();
+        
+        public void OnPrimaryClick(Unit selectedUnit)
         {
+            foreach (Unit unit in Units.Where(u => u.Selected))
+            {
+                unit.Deselect();
+            }
+            selectedUnit.Select();
+        }
 
+        public void OnPrimaryHold(Vector2 selectionStart, Vector2 selectionEnd)
+        {
+            IEnumerable<Unit> unitsToSelect = Units.Where(u => CheckSelectArea(u.transform.position, selectionStart, selectionEnd));
+
+            Unit SelectedUnit = Units.Where(u => u.Selected).FirstOrDefault();
+            if (SelectedUnit != null)
+            {
+                unitsToSelect = unitsToSelect.Where(u => u.Owner == SelectedUnit.Owner);
+            }
+
+            foreach (Unit unit in unitsToSelect)
+            {
+                unit.Select();
+            }
+        }
+
+        private static bool CheckSelectArea(Vector3 unit, Vector2 selectionStart, Vector2 selectionEnd)
+        {
+            if (unit.x < selectionStart.x || unit.x > selectionStart.x)
+            {
+                return false;
+            } 
+            if (unit.y < selectionEnd.y || unit.y > selectionEnd.y)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void OnSecondaryClick(Vector2 destination)
+        {
+            IEnumerable<Unit> unitsToMove = Units.Where(u => 
+                u.Owner == UnitOwner.Player &&
+                u.Selected
+            );
+
+            foreach (Unit unit in unitsToMove)
+            {
+                unit.MoveUnit(destination);
+            }
         }
     }
 }
