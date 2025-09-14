6acbee1785e60f25d8bc66cf4c94cfa0eac2c951
VeyGudTek <90864982+VeyGudTek@users.noreply.github.com>
2025-09-14 14:14:07 -0500

    GAR-4 Implement Unit and UnitManagement (#18)
    * Set Up NavMesh Surface and Agents
    
    * Set Up Methods for Unit Selection
    
    * Switch UnitPlayer to GUID
    
    * Remove Unused State in GamePlayService
    
    * Changed UnitPlayer name, Added Initialization Method.
    
    * Rework Unit to use Composition, decouple Movement
    
    * Update Init to also have Property Checker
    
    * Create Prefabs, Have Unit ref dependencies in code
    
    * Fix MoveUnits Param, Fix Null Checks
    
    * Fix Parameter for Multiple Select
    
    * Fix Selection Logic
    
    * Update Init checker to log property type

--- Assets/Scripts/GamePlay/Unit.cs
+++ Assets/Scripts/GamePlay/Unit.cs
@@ -1,9 +1,55 @@
+using Source.Shared.Utilities;
+using System;
 using UnityEngine;
 
 namespace Source.GamePlay
 {
     public class Unit : MonoBehaviour
     {
-        private int health;
+        private UnitMovement UnitMovement;
+
+        [InitializationRequired]
+        private float? Health = null;
+        [InitializationRequired]
+        public Guid? PlayerId { get; private set; } = null;
+        public bool Selected { get; private set; } = false;
+
+        private void Awake()
+        {
+            UnitMovement = GetComponent<UnitMovement>();
+        }
+
+        private void Start()
+        {
+            this.CheckInitializeRequired();
+        }
+
+        public void Initialize(Guid playerId, float health, float speed)
+        {
+            PlayerId = playerId;
+            Health = health;
+            if (UnitMovement != null)
+            {
+                UnitMovement.Initialize(speed);
+            }
+        }
+
+        public void MoveUnit(Vector3 destination)
+        {
+            if (UnitMovement != null)
+            {
+                UnitMovement.MoveUnit(destination);
+            }
+        }
+
+        public void Select()
+        {
+            Selected = true;
+        }
+
+        public void Deselect()
+        {
+            Selected = false;
+        }
     }
 }
