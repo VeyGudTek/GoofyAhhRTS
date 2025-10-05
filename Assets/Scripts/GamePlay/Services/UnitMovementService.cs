7dd164c3210beb0ce2d369f2f09eb67f17e0914e
Tim Lin <lintim03@gmail.com>
2025-10-04 23:48:30 -0500

    custom null checker and navmesh set

--- Assets/Scripts/GamePlay/Services/UnitMovementService.cs
+++ Assets/Scripts/GamePlay/Services/UnitMovementService.cs
@@ -1,3 +1,5 @@
+using Source.Shared.Utilities;
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -5,10 +7,51 @@ namespace Source.GamePlay.Services
 {
     public class UnitMovementService : MonoBehaviour
     {
+        private enum MovementType
+        {
+            None,
+            NavMesh
+        }
+
+        [SerializeField]
+        private MovementType Movement;
+
+        [InitializationRequired]
         private NavMeshAgent NavMeshAgent;
+        [InitializationRequired]
+        private BoxCollider HitBox;
+
         private void Awake()
         {
             NavMeshAgent = GetComponent<NavMeshAgent>();
+            HitBox = GetComponent<BoxCollider>();
+        }
+
+        private void Start()
+        {
+            CustomCheckInitializeRequired();
+            SetNavmesh();
+        }
+
+        private void CustomCheckInitializeRequired()
+        {
+            try
+            {
+                this.CheckInitializeRequired();
+            }
+            catch (Exception e)
+            {
+                if (Movement == MovementType.NavMesh && e.Message.Contains(typeof(NavMeshAgent).Name))
+                {
+                    throw e;
+                }
+            }
+        }
+
+        private void SetNavmesh()
+        {
+            if (NavMeshAgent == null || HitBox == null) return;
+            NavMeshAgent.baseOffset = HitBox.size.y / 2f;
         }
 
         public void MoveUnit(Vector3 destination)
